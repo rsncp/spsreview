77e10225d4cc6ee60e5cd46ffbf2399ec5704b85
Ajmal Roshan <mohroshanajmal@gmail.com>
2020-06-04 14:30:26 +0530

    form through added

--- routes/formdisplay.js
+++ routes/formdisplay.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+let db = require('../dbconfig/db-connect');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +9,50 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  res.render('formdisplay');
+
+  let name=req.body.name;
+  let telegramId=req.body.telegramid;
+  let facebookId=req.body.facebookid;
+  let linkedIn=req.body.ldid;
+  let qualification=req.body.qualification;
+  let dateOfBirth=req.body.dob;
+  let contactNo= req.body.contact_no;
+  let alternativeNo=req.body.alt_contact_no;
+  let gender=req.body.gender;
+  let status=req.body.status;
+  let email=req.body.email;
+  let address=req.body.address;
+  let uploadFile=req.body.uploadfile;
+  let tellUs=req.body.tellus;
+
+
+  db.get().collection('user').insertOne({
+
+    name:name,
+    telegramid:telegramId,
+    facebookid:facebookId,
+    linkedinid:linkedIn,
+    qualification:qualification,
+    dob:dateOfBirth,
+    contact_no: contactNo,
+    alt_contact_no:alternativeNo,
+    gender:gender,
+    status:status,
+    email:email,
+    address:address,
+    uploadfile:uploadFile,
+    tellus:tellUs
+
+
+
+  } ,function (err,user) {
+   if (err){
+     console.log('User data insertion failed');
+   }else {
+     console.log('User data inserted');
+     res.redirect('/admin')
+   }
+  });
 });
 
 
